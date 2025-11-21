cbb0ece1af547718c053ccd204dda80791149c45
doki <doki@dokideMac-mini.local>
2025-11-21 18:53:30 +0800

    update set tools

--- tools/set.go
+++ tools/set.go
@@ -14,17 +14,34 @@ func NewSet[T comparable](items ...T) *set[T] {
 	return s
 }
 
-func (s *set[T]) Add(item T) {
-	s.m[item] = struct{}{}
+func (s *set[T]) Add(items ...T) *set[T] {
+	for _, item := range items {
+		s.m[item] = struct{}{}
+	}
+	return s
 }
 
-func (s *set[T]) Remove(item T) {
+func (s *set[T]) Remove(item T) *set[T] {
 	delete(s.m, item)
+	return s
 }
 
 func (s *set[T]) Contains(item T) bool {
 	_, ok := s.m[item]
 	return ok
+}
+
+func (s *set[T]) Clear() *set[T] {
+	s.m = make(map[T]struct{})
+	return s
+}
+
+func (s *set[T]) Size() int {
+	return len(s.m)
+}
+
+func (s *set[T]) IsEmpty() bool {
+	return len(s.m) == 0
 }
 
 func (s *set[T]) Slice() []T {
